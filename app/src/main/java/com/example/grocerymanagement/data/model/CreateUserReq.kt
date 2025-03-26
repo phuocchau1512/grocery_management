f6f3945bc65e34934c3c05d81497703941c8ab7d
phuocchau1512 <142923216+phuocchau1512@users.noreply.github.com>
2025-03-26 23:57:44 +0700

    layout List

--- app/src/main/java/com/example/grocerymanagement/data/model/CreateUserReq.kt
+++ app/src/main/java/com/example/grocerymanagement/data/model/CreateUserReq.kt
@@ -5,7 +5,6 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-@Serializable
 data class CreateUserReq(val name: String, val email: String, val password: String) {
     fun toJson(): String {
         return Json.encodeToString(this)
--- app/src/main/java/com/example/grocerymanagement/presentation/fragments/ListItemFragment.kt
+++ app/src/main/java/com/example/grocerymanagement/presentation/fragments/ListItemFragment.kt
@@ -6,10 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.grocerymanagement.R
-
+import com.example.grocerymanagement.databinding.FragmentEditItemBinding
+import com.example.grocerymanagement.databinding.FragmentListItemBinding
+import com.example.grocerymanagement.presentation.viewModel.EditProductViewModel
+import com.example.grocerymanagement.presentation.viewModel.InventoryViewModel
 
 
 class ListItemFragment : Fragment() {
+
+    private var _binding: FragmentListItemBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: InventoryViewModel  // Khai báo ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
